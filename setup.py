fd00cecb626a7af0ffbeacde424878995aaa8e00
Tres Walsh <tres.walsh@mnmlstc.com>
2012-02-13 15:38:18 -0800

    Modified setup.py to read from setup.cfg
    Added 'sphinx' run to tox.ini
    Added .tox/ to .gitignore

--- setup.py
+++ setup.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python
 
+from configparser import ConfigParser
 from setuptools import setup
 
 import sys
 import os
 
+config = ConfigParser()
+config.read(os.path.join(os.getcwd(), 'setup.cfg'))
+metadata = config['metadata']
+files = config['files']
+
 setup(
     # Config information
-    name='bit',
-    version='0.4',
-    license='BSD',
-    author='Tres Walsh',
-    url='http://mnmlstc.com',
-    description='A minimal build system',
-    keywords='bit build-system build-tools minimal small build',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Software Development :: Build Tools',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: POSIX :: BSD',
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: Implementation :: CPython'
-    ],
+    name=metadata['name'],
+    version=metadata['version'],
+    license=metadata['license'],
+    author=metadata['author'],
+    url=metadata['home-page'],
+    description=metadata['summary'],
+    keywords=metadata['keywords'],
+    classifiers=metadata['classifiers'].split('\n')[1:],
     install_requires=['distribute'],
     setup_requires=['nose>=1.1.2'],
     test_suite = 'nose.collector',
     # Content/Data
-    packages=[
-        'bit',
-        'bit.core'
-    ],
+    packages=files['packages'].split('\n')[1:],
     entry_points=dict(console_scripts=['bit=bit:main']),
     # Additional Info
     zip_safe=True
