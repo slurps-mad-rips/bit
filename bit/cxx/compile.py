e0549f93207b4e1a403271fd8693897ffc0ffca9
Tres Walsh <tres.walsh@mnmlstc.com>
2012-03-16 16:32:38 -0700

    Added attributes to compile and link tasks

--- bit/cxx/compile.py
+++ bit/cxx/compile.py
@@ -1,4 +1,16 @@
 from bit.core.task import Task
 
 class CXXCompile(Task):
-    pass
+    
+    compiler = None # Currently don't have a default compiler set...
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.attributes.update(dict(
+            cxxflags=[],
+            include=[],
+            library=[],
+            defines=[],
+            cflags=[],
+            flags=[]
+        ))
--- bit/cxx/link.py
+++ bit/cxx/link.py
@@ -1,4 +1,13 @@
 from bit.core.task import Task
 
 class CXXLink(Task):
-    pass
+    linker = None # No 'linker' is set.
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.attributes.update(dict(
+            library[],
+            flags=[],
+            type=[] # We will 'pop' whatever the type is from the list, so no
+                    # worries :v
+        ))
